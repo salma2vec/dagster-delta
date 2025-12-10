1df7ddbe86971b94b490400f784339bd15568e6a
Ion Koutsouris <15728914+ion-elgreco@users.noreply.github.com>
2025-12-10 16:19:45 +0100

    test: add multiple single quote case

--- libraries/dagster-delta/dagster_delta_tests/test_create_predicate.py
+++ libraries/dagster-delta/dagster_delta_tests/test_create_predicate.py
@@ -2,8 +2,8 @@ from dagster_delta._handler.utils.predicates import create_predicate
 
 
 def test_create_predicate_string_list_proper_escaping():
-    filters = [("country", "IN", ["foo", "bar", "ba'z"])]
+    filters = [("country", "IN", ["foo", "bar", "ba'z", "ba''z"])]
 
     pred = create_predicate(filters)
 
-    assert pred == "country IN ('foo', 'bar', 'ba''z')"
+    assert pred == "country IN ('foo', 'bar', 'ba''z', 'ba''''z')"
